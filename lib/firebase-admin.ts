a0d127471cbd79da15b67cafff4ffde6b2c302b5
Shai Lev <shai200@gmail.com>
2026-02-05 23:56:57 +0200

    db init

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -19,4 +19,4 @@ if (!admin.apps.length) {
 }
 
 export const adminApp = admin.app();
-export const adminDb = admin.firestore(adminApp, 'omnipay-users');
+export const adminDb = admin.firestore(undefined, 'omnipay-users');
